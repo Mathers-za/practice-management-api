a6b2677f39b681258576db206079d2ee0d73c1ea
Daniel <danielmathers97@gmail.com>
2024-07-16 13:28:22 +0200

    add invoice stats to financial statistics endpoint

--- routes/statistics.js
+++ routes/statistics.js
@@ -15,7 +15,7 @@ router.get(
     const { start_date, end_date } = req.query;
     const profileId = req.params.id;
     try {
-      const result = await pool.query(
+      const appointmentsResult = await pool.query(
         `select count(*) as total_num_appointments, sum(total_amount) as appointments_amount_total
 , sum(amount_paid) as appointments_amount_paid, sum(amount_due) as appointments_amount_due, sum(discount) as appointments_total_discounts
 from financials
@@ -27,12 +27,29 @@ where appointments.soft_delete = false and user_profile.id = $1 and appointment_
         [profileId, end_date, start_date]
       );
 
+      const invoiceResults = await pool.query(
+        `SELECT  count(*) as total_num,
+    SUM(CASE WHEN invoice_status = 'Paid' THEN 1 ELSE 0 END) AS num_invoices_Paid,
+	sum(case when invoice_status = 'Sent' then 1 else 0 end) as num_invoices_sent,
+	sum( case when invoice_status = 'In progress' then 1 else 0 end) as num_invoices_in_progress
+FROM invoices
+JOIN appointments ON appointments.id = invoices.appointment_id
+JOIN patients ON patients.id = appointments.patient_id
+WHERE patients.profile_id = $1 and appointment_date <= $2 and appointment_date >= $3 `,
+        [profileId, end_date, start_date]
+      );
+
       res.status(200).json({
-        appointmentCount: result.rows[0].total_num_appointments,
-        totalValue: result.rows[0].appointments_amount_total,
-        totalCollected: result.rows[0].appointments_amount_paid,
-        totalOutstanding: result.rows[0].appointments_amount_due,
-        totalDiscount: result.rows[0].appointments_total_discounts,
+        appointmentCount: appointmentsResult.rows[0].total_num_appointments,
+        totalValue: appointmentsResult.rows[0].appointments_amount_total,
+        totalCollected: appointmentsResult.rows[0].appointments_amount_paid,
+        totalOutstanding: appointmentsResult.rows[0].appointments_amount_due,
+        totalDiscount: appointmentsResult.rows[0].appointments_total_discounts,
+        totalInvoicesPaid: invoiceResults.rows[0].num_invoices_paid,
+        totalInvoicesSent: invoiceResults.rows[0].num_invoices_sent,
+        totalInvoices: invoiceResults.rows[0].total_num,
+        totalInvoicesInProgress:
+          invoiceResults.rows[0].num_invoices_in_progress,
       });
     } catch (error) {
       res
